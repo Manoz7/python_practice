5ea63e86028a74a544959863d182b89ba45db715
Manoz7 <manojkhanal936@gmail.com>
2026-05-01 12:54:40 -0500

    Minor Bug Fixes

--- largest.py
+++ largest.py
@@ -1,13 +1,13 @@
-# num1 = int(input("Enter a number:"))
-# num2 = int(input("Enter a number:"))
-# num3 = int(input("Enter a number:"))
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+num3 = int(input("Enter a number:"))
 
-# if num1>num2 and num1>num3:
-#     print(f"{num1} is largest")
-# elif num2>num1 and num2>num3:
-#     print(f"{num2} is largest")
-# else:
-#     print(f"{num3} is largest")
+if num1>num2 and num1>num3:
+    print(f"{num1} is largest")
+elif num2>num1 and num2>num3:
+    print(f"{num2} is largest")
+else:
+    print(f"{num3} is largest")
 
 array = []
 
